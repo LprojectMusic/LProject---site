8ee67111fdb387f493b3039436415102422fc5fc
LprojectMusic <nicolasbore@hotmail.com>
2025-09-28 17:06:07 +0200

    Le site

---
+++ App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ color: 'white', backgroundColor: 'black', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+      <h1>L PROJECT</h1>
+      <p>Hip Hop Minded Music</p>
+    </div>
+  );
+}
