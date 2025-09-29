5eaaf4357a5e826f37ce360d6c62c95ffb55bada
Nico Mrb <nicolasbore@orange.fr>
2025-09-29 11:26:41 +0200

    Fichier vite.config.js corrigé

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()]
-})
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
 });
->>>>>>> 8ee67111fdb387f493b3039436415102422fc5fc
